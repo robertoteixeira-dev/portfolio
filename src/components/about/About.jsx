42ec60d5b22c540a892bd2b84c28ae643d71a483
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-05-11 14:15:17 -0300

    about circle

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './About.css'
 import ME from '../../assets/me-about.jpg'
+import Typical from 'react-typical'
 import { FaAward } from 'react-icons/fa'
 import { FaGraduationCap } from 'react-icons/fa'
 import { BsLaptopFill } from 'react-icons/bs'
@@ -13,10 +14,29 @@ const About = () => {
       <h2>About Me</h2>
 
       <div className="container about_container">
-        <div className="about_me">
-          <div className="about_me-image">
-            <img src={ME} alt="About Image" />
-          </div>
+
+        <div className="about_tech">
+
+          <img src={ME} alt='' />
+
+          <h5 className="personality">
+            {" "}
+            <Typical
+              loop={Infinity}
+              steps={[
+                "Self-Learner",
+                1000,
+                "Hard Worker",
+                1000,
+                "Team Player",
+                1000,
+                "Organized",
+                1000,
+                "Professional Coder",
+                1000,
+              ]}
+            />
+          </h5>
         </div>
 
         <div className="about_content">
@@ -31,7 +51,7 @@ const About = () => {
             <article className="about_card_edu">
               <FaGraduationCap className="about_icon" />
               <h5>Education</h5>
-              <small><BsPatchCheckFill /> Computer Science</small><br/>
+              <small><BsPatchCheckFill /> Computer Science</small><br />
               <small><BsPatchCheckFill /> IT Technician</small>
             </article>
 
@@ -43,14 +63,15 @@ const About = () => {
           </div>
 
           <p>
-          I'm a self-directed, action-oriented Full-Stack Developer passionate about technology and software development. Graduated as IT technician and currently a Computer Science Studant with ability to collect and manage information efficiently and accurately. 
-           Good object-oriented programming knowledge. Great written and verbal communications skills in all the three languages that I speak (English, Spanish and Portuguese) and a strong desire to work hard and perform well. 
-          Engaged in the search for suitable solutions and specs that best meet the needs of the project.
+            I'm a self-directed, action-oriented Full-Stack Developer passionate about technology and software development. Graduated as IT technician and currently a Computer Science Studant with ability to collect and manage information efficiently and accurately.
+            Good object-oriented programming knowledge. Great written and verbal communications skills in all the three languages that I speak (English, Spanish and Portuguese) and a strong desire to work hard and perform well.
+            Engaged in the search for suitable solutions and specs that best meet the needs of the project.
           </p>
 
           <a href="#contact" className="btn btn-primary">Let's Talk</a>
-
         </div>
+
+
       </div>
     </section>
   )
--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -51,7 +51,6 @@ const Project = () => {
 
       </div>
 
-
     </section>
   )
 }
